43c3911ce80a4c04bdbdf694a61afff636813ba5
Miguel Cordero <mikeycordero10@icloud.com>
2025-01-07 17:02:45 -0700

    Componente departamentos a version de escritorio

--- src/components/Departaments.tsx
+++ src/components/Departaments.tsx
@@ -20,18 +20,20 @@ export const Departaments: React.FC = () => {
 
   return (
     <section className="space-y-2">
-      <h2 className="font-semibold text-2xl">Todos los departamentos</h2>
-      <article className="flex flex-wrap justify-center text-[8px] gap-1">
+      <h2 className="font-semibold text-2xl md:text-3xl md:font-bold">
+        Todos los departamentos
+      </h2>
+      <article className="flex flex-wrap justify-center text-[8px] gap-1 md:gap-5">
         {departaments.map((item, index) => (
           <div
             key={index}
-            className="text-center bg-white border rounded-xl shadow w-[24%] h-28 flex flex-col items-center justify-around"
+            className="text-center md:text-xs md:px-3 md:py-1 bg-white border rounded-xl shadow w-[24%] md:w-[11%] h-28 md:h-40 flex flex-col items-center justify-around"
           >
-            <div className="flex flex-col items-center gap-1">
-              <img src={item.photo} alt="" />
+            <div className="flex flex-col items-center gap-2">
+              <img src={item.photo} alt="" className="md:size-12" />
               <span>{item.name}</span>
             </div>
-            <span className="bg-red-200 text-red-600 p-1 rounded">
+            <span className="bg-red-100 text-red-500 p-1 rounded">
               {item.discount}
             </span>
           </div>
@@ -40,15 +42,15 @@ export const Departaments: React.FC = () => {
           onClick={() => {
             setShowAll((prev) => !prev);
           }}
-          className="w-[24%] flex gap-2 flex-col items-center justify-center bg-[#0266ae] rounded-xl text-white"
+          className="w-[24%] md:px-0.5 md:w-[11%] flex gap-2 flex-col items-center justify-center bg-[#0266ae] rounded-xl text-white"
         >
           {showAll ? (
             <FaAngleUp className="text-xl" />
           ) : (
             <FaAngleDown className="text-xl" />
           )}
-          <span className="text-center">
-            {showAll ? "Ver menos" : "Descubre todos los departamentos"}
+          <span className="text-center md:text-base">
+            {showAll ? "Ver menos" : "Todos los departamentos"}
           </span>
         </button>
       </article>
